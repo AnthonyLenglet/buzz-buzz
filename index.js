868658b8793202225d84967eb797e546888369da
Elodie62 <elodie602@free.fr>
2023-05-29 22:56:08 +0200

    add button random sound + add sound in localstorage

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 const buzzerSound = document.querySelector("#sound");
-setDefaultSound();
-
 const buzzerBtn = document.querySelector("#buzzer");
+const clearBtn = document.querySelector("#clear");
+
+if (localStorage.getItem("buzzSound")) {
+  buzzerSound.value = localStorage.getItem("buzzSound");
+} else {
+  const defaultSound = buzzerSound.children.item(Math.floor(Math.random() * buzzerSound.children.length));
+  buzzerSound.value = defaultSound.value;
+}
+
+buzzerSound.addEventListener("change", (event) => {
+  localStorage.setItem("buzzSound", event.target.value);
+});
+
 buzzerBtn.addEventListener("mousedown", () => {
   const buzzSound = new Audio(`./buzzes/${buzzerSound.value}.mp3`);
   buzzSound.play();
 });
 
-function setDefaultSound() {
-  const defaultSound = buzzerSound.children.item(
-    Math.floor(Math.random() * buzzerSound.children.length)
-  );
+clearBtn.addEventListener("mousedown", () => {
+  const defaultSound = buzzerSound.children.item(Math.floor(Math.random() * buzzerSound.children.length));
   buzzerSound.value = defaultSound.value;
-}
+  localStorage.setItem("buzzSound", defaultSound.value);
+});
