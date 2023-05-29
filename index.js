940c4c8217296f13478bdd929ab00d848261cdc0
AnthonyLenglet <anthony.lenglet@hotmail.com>
2023-05-29 22:27:10 +0200

    add setDefaultSound function

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const buzzerSound = document.querySelector("#sound");
-const defaultSound = buzzerSound.children.item(
-  Math.floor(Math.random() * buzzerSound.children.length)
-);
-defaultSound.setAttribute("selected", "");
+setDefaultSound();
 
 const buzzerBtn = document.querySelector("#buzzer");
 buzzerBtn.addEventListener("mousedown", () => {
   const buzzSound = new Audio(`./buzzes/${buzzerSound.value}.mp3`);
   buzzSound.play();
 });
+
+function setDefaultSound() {
+  const defaultSound = buzzerSound.children.item(
+    Math.floor(Math.random() * buzzerSound.children.length)
+  );
+  buzzerSound.value = defaultSound.value;
+}
